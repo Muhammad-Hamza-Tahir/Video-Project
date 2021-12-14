78034c8ce09427cc6753ab780a25deda9f8e92a7
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2021-12-14 11:29:51 +0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,13 +1,20 @@
 let image = document.querySelector('.loder');
+let hide = document.querySelector('.pre-loder');
 let center2 = document.querySelector('.center2')
-let play = document.querySelector('.play');
-let pause = document.querySelector('.pause');
+let play = document.querySelector('.play1');
+let pause = document.querySelector('.pause1');
 let center = document.querySelector('.center');
-window.addEventListener('load',function () {
-     image.classList.add('abc')
-     center2.classList.add('ab')
+window.addEventListener('load',function (event) {
+    event.preventDefault()
+    hide.classList.add('ppp')
 
 })
-pause.addEventListener('click',function () {
-    video.pause();
+play.addEventListener('click',function () {
+    if (play.classList.contains('switch1')) {
+        play.classList.remove('switch1');
+        Video.pause();
+    }else{
+    play.classList.add('switch1');
+    Video.play();
+    }
 })
